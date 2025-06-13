95bbaa9f294e8f18a252706274970db008deb60a
Khanh <ngiakhanh18@gmail.com>
2025-06-13 08:48:47 -0500

    fixed light mode

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
         {/* Center Part */}
         <div className="flex-1 flex flex-col items-center justify-center md:gap-45 lg:gap-45">
           <div className="text-center">
-            <h2 className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white  md:text-4xl lg:text-7xl font-sans md:py-5 relative z-20 font-bold tracking-tight text-6xl pb-3">
+            <h2 className="bg-clip-text text-transparent bg-gradient-to-b  from-neutral-600 to-white  md:text-4xl lg:text-7xl font-sans md:py-5 relative z-20 font-bold tracking-tight text-6xl pb-3">
               Admed Ali, <br /> Digital Orbit.
             </h2>
             <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400">
@@ -33,3 +33,4 @@ export default function Page() {
 
 // Get the best advices from our experts, including expert artists,
 // painters, marathon enthusiasts and RDX, totally free.
+// from-neutral-900 to-neutral-700
